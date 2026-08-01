5c346011a0a07508c39ba64f401a42a538f05e0a
Hitesh Mahto <hiteshmahto786@gmail.com>
2026-08-01 12:33:25 +0530

    KI-2 Adding initial Java project and build script for testing

---
+++ src/com/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("Hello from Kobee! The build was successful.");
+        System.out.println("==========================================");
+    }
+}
